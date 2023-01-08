57cdb0c408fff6ba961cf8bc103386be87b7bed8
nika <nika.gogua8@gmail.com>
2023-01-08 21:31:28 +0400

    starting files for project

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        barlow: ["barlow", "sans-serif"],
+        bellefair: ["bellefair", "sans-serif"],
+      },
+      colors: {
+        "space-black": "#0B0D17",
+        "space-gray": "#D0D6F9",
+      },
+    },
+  },
+  plugins: [],
+};
